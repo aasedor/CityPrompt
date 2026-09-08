c0271cef3294c6d793c2005c3e99f3cb64846ed1
aasedor <andrew.sedor@gmail.com>
2026-09-07 18:02:08 -0600

    test: satisfy lint for public-road connection cases

--- backend/tests/test_public_road_connection.py
+++ backend/tests/test_public_road_connection.py
@@ -16,7 +16,15 @@ def test_public_connection_keeps_parcel_and_cross_section():
 @pytest.mark.parametrize("change", ["disabled", "building", "far", "two_ends", "too_wide", "reentry"])
 def test_public_connection_does_not_bypass_other_site_constraints(change):
     props, polygon = fixture(140 if change == "far" else 110, change != "disabled")
-    if change == "two_ends": props["plan_centerline"][0] = [-10 / 111320, 50 / 111320]
-    if change == "too_wide": polygon = polygon.buffer(5 / 111320)
-    if change == "reentry": props["plan_centerline"] = [[10 / 111320, 50 / 111320], [110 / 111320, 50 / 111320], [20 / 111320, 50 / 111320], [110 / 111320, 50 / 111320]]
+    if change == "two_ends":
+        props["plan_centerline"][0] = [-10 / 111320, 50 / 111320]
+    if change == "too_wide":
+        polygon = polygon.buffer(5 / 111320)
+    if change == "reentry":
+        props["plan_centerline"] = [
+            [10 / 111320, 50 / 111320],
+            [110 / 111320, 50 / 111320],
+            [20 / 111320, 50 / 111320],
+            [110 / 111320, 50 / 111320],
+        ]
     assert not public_road_connection_fits("building" if change == "building" else "road", props, polygon, box(0, 0, 100 / 111320, 100 / 111320))
